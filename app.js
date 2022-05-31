d6a963507b3ec378059336f3061593ac7a00c1f1
imaishika <1905444@kiit.ac.in>
2022-06-01 00:07:13 +0530

    added mongo db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 console.log(date);
 const app = express();
@@ -12,36 +13,119 @@ app.set('view engine', 'ejs');
 
 const port = (process.env.PORT || 3000);
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
+const itemSchema = {
+    name: {
+      type: String,
+      required: [true, "Task name required, please specify a name"]
+    }
+};
 
-let items = ["Leetcode", "GFG", "Core Domain"];
-let workItems = [];
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo-list"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req, res) {
 
-    let day = date();
+    //let day = date();
+    Item.find({}, function(err, foundItems) {
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+      if(foundItems.length === 0) {
 
+        Item.insertMany(defaultItems, function (err) {
+            if(err) {
+              console.log(err);
+            } else {
+              console.log("Successfully saved default items to DB!");
+            }
+          });
+
+          res.redirect("/");
+      }
+      else {
+          res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }
+
+    });
 });
 
-app.get("/work", function(req, res) {
-  res.render("list", {kindOfDay: "Work", newListItems: workItems});
+app.get("/:customListName", function(req, res) {
+    const customListName = req.params.customListName;
+    console.log(customListName);
+
+    List.findOne({name: customListName}, function(err, foundList) {
+        if(!err) {
+            if(!foundList) {
+              //create a new list
+              const list = new List ({
+                  name: customListName,
+                  items: defaultItems
+              });
+
+              list.save();
+
+              res.redirect("/" + customListName);
+            } else {
+                //show an existing list
+                res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+            }
+        }
+    });
+});
+
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.post("/", function(req, res){
+    const itemName = req.body.newItem;
+    const listName = req.body.listName;
+
+    const item = new Item ({
+      name: itemName
+    });
+
+    if(listName === "Today") {
+      item.save();
+      res.redirect("/");
+    } 
+    else {
+      List.findOne({name: listName}, function (err, foundList) {
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      });
+    }
+});
+
+
+app.post("/delete", function(req, res) {
+  const id = req.body.deleteItem;
+
+  Item.findByIdAndRemove(id, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  })
 })
-
-app.post("/", function (req, res){
-  let item = req.body.newItem;
-
-  if(req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-
-});
-
 app.listen(port, function() {
   console.log("Server listening to port " + port);
 });
